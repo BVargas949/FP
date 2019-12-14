cd8b4985cf08400f446c6efd39a2f01ac75f5149
Bryan Vargas <BVargas949@gmail.com>
2019-12-13 17:03:17 -0700

    Placed all previous trial work onto this respository

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+const fetch = require('node-fetch');
+const path = require('path');
+var app = express();
+const port = 8080;
+
+app.set('view engine', 'pug');
+app.set("views", path.join(__dirname,"views"));
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+
+
+
+app.get('/', (req, res) => {  
+
+    fetch('https://swapi.co/api/planets/3/')
+    .then(res => res.json())
+    .then(
+      (json) => {
+        console.log(json)
+      res.render('index', { 
+          planet: json
+        })
+      })
+    });
+
+
+
+    const server = app.listen(port, () =>{
+        console.log(`Express running - PORT ${server.address().port}`)
+      })
